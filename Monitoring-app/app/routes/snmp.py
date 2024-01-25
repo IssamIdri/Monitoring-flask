6d23d0c3995fd9d134a2c5b7af4683b25c211e75
Anas-Bou <130944099+Anas-Bou@users.noreply.github.com>
2024-01-25 17:10:21 +0100

    snmp reseau

---
+++ Monitoring-app/app/routes/snmp.py
@@ -0,0 +1,22 @@
+from flask import request, render_template, redirect
+
+from app.data.services.deviceservices import DeviceService
+from app import app
+from app.data.snmp_data.get_information_snmp import snmp_get 
+
+
+@app.route('/snmp', methods=['GET', 'POST'])
+def snmp_page():
+    if request.method == 'POST':
+        # Extract form data
+        target = request.form.get('target')
+        oid = request.form.get('oid')
+        community = request.form.get('community', 'public')
+        port = int(request.form.get('port', 161))
+
+        # Perform SNMP GET request
+        snmp_data = snmp_get(oid, target, community, port)
+        return render_template('snmp.html', snmp_data=snmp_data)
+    else:
+        # Render the SNMP form page
+        return render_template('snmp.html', snmp_data=None)
