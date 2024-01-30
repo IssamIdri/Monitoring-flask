38bce48b0ad17d0ede90fb0f9f1a70683a7ea2d9
Anas-Bou <130944099+Anas-Bou@users.noreply.github.com>
2024-01-30 14:20:56 +0100

    index map markers

--- Monitoring-app/app/routes/index.py
+++ Monitoring-app/app/routes/index.py
@@ -1,8 +1,15 @@
 from flask import request, render_template
 from app import app
+from flask import request, render_template
+from app.data.IOTservice.iotservice import iotdevice
+from app import app
 
 
-
-@app.route('/index')
+@app.route('/index', methods=["GET"])
 def index():
-    return render_template('index.html')
+    if request.method == "GET":
+        device_ser = iotdevice()
+        result_iotdata = device_ser.getalliotdevices()
+        print(result_iotdata)
+        return render_template("index.html", devices_data=result_iotdata)
+    
